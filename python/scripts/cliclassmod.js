d6c99ba6789130f767ba8fe6b6281dc0964fa073
devilinside71 <devilinside71@users.noreply.github.com>
2018-03-18 19:33:33 +0300

    CLI main module

--- python/scripts/cliclassmod.js
+++ python/scripts/cliclassmod.js
@@ -7,10 +7,14 @@ $(document).ready(function () {
         $("#tableargs").find('tbody')
             .append($('<tr>')
                 .append($('<td class="Col1">Argument' + i + ':</td>'))
-                .append($('<td class="Col2"><input id="argsSh' + i + '" type="text" class="full" /></td>'))
-                .append($('<td class="Col1"><input id="args' + i + '" type="text" class="full" /></td>'))
-                .append($('<td class="half"><input id="help' + i + '" type="text" class="full" /></td>'))
-                .append($('<td class="Col2"><input id="argscheck' + i + '" type="checkbox" />Boolean</td>'))
+                .append($('<td class="Col2"><input id="argsSh' + i +
+                    '" type="text" class="full" /></td>'))
+                .append($('<td class="Col1"><input id="args' + i +
+                    '" type="text" class="full" /></td>'))
+                .append($('<td class="half"><input id="help' + i +
+                    '" type="text" class="full" /></td>'))
+                .append($('<td class="Col2"><input id="argscheck' + i +
+                    '" type="checkbox" />Boolean</td>'))
             );
     }
     for (i = 1; i < funcNum + 1; i++) {
@@ -22,7 +26,7 @@ $(document).ready(function () {
                 .append($('<td>&nbsp;</td>'))
             );
     }
-
+    addDefaults();
     $('#generate').click(function () {
         generateModules();
     });
@@ -32,6 +36,8 @@ $(document).ready(function () {
 });
 
 function generateModules() {
+
+    generateFilenames();
     generateMain();
 }
 
@@ -39,8 +45,21 @@ function resetNames() {
 
 }
 
+function generateFilenames() {
+    $('#mainName').val($('#ClassName').val().toLowerCase() + '.py');
+    $('#testName').val($('#ClassName').val().toLowerCase() + '_test.py');
+    $('#notesName').val($('#ClassName').val().toLowerCase() + '_notes.txt');
+}
+
+function addDefaults() {
+    $('#argsSh1').val('i');
+    $('#args1').val('input_file');
+    $('#help1').val('input file name');
+}
+
 function generateMain() {
     var textString = "";
+    var i = 1;
     textString = textString + '# -*- coding: utf-8 -*-\n';
     textString = textString + '"""\n';
     textString = textString + 'This module deals with code.\n';
@@ -52,7 +71,8 @@ function generateMain() {
     textString = textString + '\n';
     textString = textString + '\n';
     textString = textString + '__author__ = "Laszlo Tamas"\n';
-    textString = textString + '__copyright__ = "Copyright (c) 2048 Laszlo Tamas"\n';
+    textString = textString +
+        '__copyright__ = "Copyright (c) 2048 Laszlo Tamas"\n';
     textString = textString + '__licence__ = "MIT"\n';
     textString = textString + '__version__ = "1.0"\n';
     textString = textString + '\n';
@@ -66,7 +86,7 @@ function generateMain() {
     textString = textString + '        pass\n';
     textString = textString + '\n';
     textString = textString + '    def sample_func(self):\n';
-    textString = textString + '        pass\n';    
+    textString = textString + '        pass\n';
     textString = textString + '\n';
     textString = textString + '\n';
     textString = textString + 'def parse_arguments():\n';
@@ -77,16 +97,39 @@ function generateMain() {
     textString = textString + '    """\n';
     textString = textString + '    parser = argparse.ArgumentParser()\n';
 
-    textString = textString + '    parser.add_argument(' + "'" + '-d' + "'" + ', ' + "'" + '--directory' + "'" + ', help=' + "'" + 'ZPL template folder' + "'" + ')\n';
-    textString = textString + '    parser.add_argument(' + "'" + '-l' + "'" + ', ' + "'" + '--label' + "'" + ', help=' + "'" + 'ZPL label template' + "'" + ')\n';
-    textString = textString + '    parser.add_argument(' + "'" + '-t' + "'" + ', ' + "'" + '--text' + "'" + ', help=' + "'" + 'input text' + "'" + ')\n';
+    for (i = 1; i < argNum + 1; i++) {
+        if ($('#argsSh' + i).val() != "") {
+            var action = "";
+            if ($('#argscheck' + i).prop('checked')) {
+                action = ', action=' + "'" + 'store_true' + "'";
+            }
+            textString = textString + '    parser.add_argument(' + "'" + '-' +
+                $('#argsSh' + i).val() + "'" + ', ' + "'" + '--' +
+                $('#args' + i).val() + "'" + action + ', help=' + "'" +
+                $('#help' + i).val() + "'" + ')\n';
+        }
+    }
 
-    textString = textString + '    parser.add_argument(' + "'" + '-f' + "'" + ', ' + "'" + '--function' + "'" + ', type=str,\n';
-    textString = textString + '                        choices=[' + "'" + 'convert_utf8' + "'" + '],\n';
-    textString = textString + '                        help=' + "'" + 'function to execute' + "'" + ')\n';
+    textString = textString + '    parser.add_argument(' + "'" + '-f' +
+        "'" + ', ' + "'" + '--function' + "'" + ', type=str,\n';
 
-    textString = textString + '    parser.add_argument(' + "'" + '-v' + "'" + ', ' + "'" + '--verbose' + "'" + ', action=' + "'" + 'store_true' + "'" + ',\n';
-    textString = textString + '                        help=' + "'" + 'increase output verbosity' + "'" + ')\n';
+    textString = textString + '                        choices=[\n';
+    for (i = 1; i < funcNum + 1; i++) {
+        if ($('#funcs' + i).val() != "") {
+            textString = textString + '                            ' +
+                "'" + $('#funcs' + i).val() + "'," + '\n';
+        }
+    }
+    textString = textString + '                        ],\n';
+
+    textString = textString + '                        help=' + "'" +
+        'function to execute' + "'" + ')\n';
+
+    textString = textString + '    parser.add_argument(' + "'" + '-v' +
+        "'" + ', ' + "'" + '--verbose' + "'" +
+        ', action=' + "'" + 'store_true' + "'" + ',\n';
+    textString = textString + '                        help=' +
+        "'" + 'increase output verbosity' + "'" + ')\n';
     textString = textString + '    return parser.parse_args()\n';
 
     textString = textString + '\n';
@@ -95,13 +138,26 @@ function generateMain() {
     textString = textString + '    """Execute the program by arguments.\n';
     textString = textString + '    """\n';
     textString = textString + '    args = parse_arguments()\n';
+    textString = textString + '    ret = None\n';
 
-    textString = textString + '    if args.function == ' + "'" + 'convert_utf8' + "'" + ':\n';
-    textString = textString + '        pass\n';
-
+    for (i = 1; i < funcNum + 1; i++) {
+        if ($('#funcs' + i).val() != "") {
+            textString = textString + '    if args.function == ' + "'" +
+                $('#funcs' + i).val() + "'" + ':\n';
+            textString = textString + '        if args.verbose:\n';
+            textString = textString + '            ret = ' +
+                $('#ClassName').val().toUpperCase() + '.sample_func()\n';
+            textString = textString + '        else:\n';
+            textString = textString + '            ret = ' +
+                $('#ClassName').val().toUpperCase() + '.sample_func()\n';
+        }
+    }
     textString = textString + '\n';
-    textString = textString + 'if __name__ == ' + "'" + '__main__' + "'" + ':\n';
-    textString = textString + '    ' + $('#ClassName').val().toUpperCase() + ' = ' + $('#ClassName').val() + '()\n';
+    textString = textString + '\n';
+    textString = textString + 'if __name__ == ' + "'" +
+        '__main__' + "'" + ':\n';
+    textString = textString + '    ' + $('#ClassName').val().toUpperCase() +
+        ' = ' + $('#ClassName').val() + '()\n';
     textString = textString + '    execute_program()\n';
     textString = textString + '    sys.exit()\n';
 
