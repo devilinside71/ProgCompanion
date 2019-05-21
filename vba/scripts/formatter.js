7a537e3cf84b304aa790fc9b5462be7549842cae
devilinside71 <devilinside71@users.noreply.github.com>
2019-05-21 13:52:25 +0200

    Update formatter

--- vba/scripts/formatter.js
+++ vba/scripts/formatter.js
@@ -34,12 +34,48 @@ function formatVBA() {
         "Option Compare", "Option Explicit", "Option Private",
         "Print #", "Private", "Property Get", "Property Let",
         "Property Set", "Public", "Put", "RaiseEvent",
-        "Randomize", "ReDim", "Rem", "Reset",
+        "Randomize", "ReDim", "REM", "Reset",
         "Resume", "RmDir", "SaveSetting", "Seek",
         "Select Case", "SendKeys", "Set", "SetAttr",
         "Static", "Stop", "Sub", "Time",
         "Type", "Unload", "While", "Wend",
-        "Width #", "With", "Write #"
+        "Width #", "With", "Write #",
+        "End Sub", "End Function",
+        "Debug.Print", "MsgBox"
+    );
+    var funcsUp = new Array(
+        "Abs", "Array", "Asc", "Atn", "CBool", "CByte",
+        "CCur", "CDate", "CDbl", "CDec", "Choose", "Chr",
+        "CInt", "CLng", "Cos", "CurDir", "CVar", "CVErr",
+        "CSng", "CStr", "Date", "DateAdd", "DateDiff", "DatePart",
+        "DateSerial", "DateValue", "Day", "DDB", "Dir", "Error",
+        "Exp", "FileAttr", "FileDateTime", "FileLen", "Filter", "Fix",
+        "Format", "FormatCurrency", "FormatDateTime", "FormatNumber", "FormatPercent", "FV",
+        "GetAttr", "Hex", "Hour", "IIf", "InputBox", "InStr",
+        "InStrRev", "Int", "IPmt", "IRR", "IsArray", "IsDate",
+        "IsEmpty", "IsError", "IsMissing", "IsNull", "IsNumeric", "IsObject",
+        "Join", "LBound", "LCase", "Left", "Len", "Log",
+        "LTrim", "Mid", "Minute", "MIRR", "Month", "MonthName",
+        "MsgBox", "Now", "NPer", "NPV", "Oct", "Pmt",
+        "PPmt", "PV", "Rate", "Replace", "Right", "Rnd",
+        "Round", "RTrim", "Second", "Sgn", "Sin", "SLN", "Space",
+        "Split", "Sqr", "Str", "StrComp", "StrConv", "String",
+        "StrReverse", "Switch", "SYD", "Tan", "Time", "Timer",
+        "TimeSerial", "TimeValue", "Trim", "UBound", "UCase", "Val",
+        "Weekday", "WeekdayName", "Year",
+        "addItem", "getCellRangeByName", "getCellByPosition", "getByName",
+        "setActiveSheet", "Worksheets", "Sheets", "findSheetIndex", "InsertNewByName",
+        "LoadLibrary", "getURL", "DirectoryNameoutofPath"
+    );
+    var typesUp = new Array(
+        " As String", " As Integer", " As Double",
+        " As WorkSheet", " As WorkBook",
+        " As Object"
+    );
+    var objectsUp = new Array(
+        "ThisComponent", "CurrentController",
+        "ActiveSheet", "ActiveWorkbook", "GlobalScope",
+        "BasicLibraries"
     );
     var commandsBefore = new Array("", "-", "-", "-",
         "0", "0", "0", "0", "0", "0",
@@ -85,13 +121,25 @@ function formatVBA() {
             }
 
         }
-        //make uppercase
+        //make uppercase statement
         for (k = 0; k < commandsUp.length; k++) {
             if ((commandsUp[k] + " ").toLowerCase() ==
                 line.slice(0, commandsUp[k].length + 1).toLowerCase().trim() +
                 " ") {
-                line = line.replace(line.slice(0, commandsUp[k].length), commandsUp[k])
+                line = line.replace(new RegExp(line.slice(0, commandsUp[k].length), 'i'), commandsUp[k])
             }
+        }
+        //make uppercase functions
+        for (k = 0; k < funcsUp.length; k++) {
+            line = line.replace(new RegExp(funcsUp[k] + '\\(', 'gi'), funcsUp[k] + '(');
+        }
+        //make uppercase types
+        for (k = 0; k < typesUp.length; k++) {
+            line = line.replace(new RegExp(typesUp[k], 'gi'), typesUp[k]);
+        }
+        //make uppercase objects
+        for (k = 0; k < objectsUp.length; k++) {
+            line = line.replace(new RegExp(objectsUp[k] + '\\.', 'gi'), objectsUp[k] + '.');
         }
         outText = outText + getIndent(beforeIndent) + line + "\n";
         if (afterIndent == 1) {
