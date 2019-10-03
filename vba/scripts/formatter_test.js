bf09361825ba97b6615a43e12f702b5715988389
devilinside71 <devilinside71@users.noreply.github.com>
2019-10-03 05:57:15 +0200

    REM line

--- vba/scripts/formatter_test.js
+++ vba/scripts/formatter_test.js
@@ -16,15 +16,29 @@ function formatVBA() {
   var i = 0;
   for (i = 0; i < lines.length; i++) {
     line = lines[i].trim();
-
+    if (isRemLine(line)) {
+      console.log('Remline: ' + line);
+      if (line.substring(0, 4).toLowerCase() === 'rem ') {
+        line = 'REM ' + line.substring(4);
+      }
+    }
     outText += line + '\n';
   }
   $('#CodeFormat').val(outText);
 }
 
+function isRemLine(line) {
+  var ret = false;
+  if (
+    line.substring(0, 1) === '\'' ||
+    line.substring(0, 4).toLowerCase() === 'rem '
+  ) {
+    ret = true;
+  }
+  return ret;
+}
 
 function clearCode() {
   $('#Code').val('');
   $('#CodeFormat').val('');
 }
-
